3acb933fc110df8d040aacafab795ce8a8d14ae3
robertsouzza <87139913+robertsouzza@users.noreply.github.com>
2021-08-03 23:22:59 -0400

    Impressão do OrderStatus

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,5 +12,11 @@ public class Program {
          Order order = new Order(1080, new Date(),OrderStatus.PENDING_PAYMENT);
          
          System.out.println(order);
+         
+         OrderStatus os1 = OrderStatus.DELIVERED;
+         OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+         
+         System.out.println(os1);
+         System.out.println(os2);
 	}
 }
--- src/entities/enums/OrderStatus.java
+++ src/entities/enums/OrderStatus.java
@@ -5,5 +5,5 @@ public enum OrderStatus {
 	PENDING_PAYMENT,
 	PROCESSING,
 	SHIPPED,
-    DEVILERED;
+    DELIVERED;
 }
